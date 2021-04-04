1e5f74e9f99b357d00f4a816dfa52d265faf1669
glukonut <razinkov@maxfungames.com>
2021-04-04 14:26:16 +0700

    drop tube by collider

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public static Camera Camera;
+
+    private void Start()
+    {
+        Camera = GetComponent<Camera>();
+    }
+}
--- Assets/Scripts/TileTubeInteraction.cs
+++ Assets/Scripts/TileTubeInteraction.cs
@@ -1,88 +1,72 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class TileTubeInteraction : MonoBehaviour
 {
-    //public UI2DSprite Sprite;
-
-    private Vector2 Position;
     private UIDragDropItem dd => GetComponent<UIDragDropItem>();
+    private UI2DSprite sprite => GetComponent<UI2DSprite>();
+    private Camera camera => CameraController.Camera;
+    
+    private Vector3 deltaPos;
+    private Vector3 pickPosition;
+    private Vector3 Position;
 
-    private UIPanel dragPanel;
     public void Start()
     {
         dd.OnStartDrag += OnStartDrag;
         dd.OnEndDrag += OnEndDrag;
-        dd.OnEndDrag += OnProcessDrag;
-        Position = Levels.ScrollPos;
+        dd.OnProcessDrag += OnProcessDrag;
     }
 
     private void OnStartDrag()
     {
-        //Debug.Log("Start drag");
-        PickTile();
-        dragPanel = this.gameObject.AddComponent<UIPanel>();
-        dragPanel.depth = 1000;
+        var mousePos = Input.mousePosition;
+        var mousePosWorld = camera.ScreenToWorldPoint(mousePos);
+        pickPosition = transform.position;
+        var tilePos = transform.localPosition;
+        tilePos.x += sprite.width;
+        tilePos.y += sprite.height;
+        var tilePosWorld = camera.ScreenToWorldPoint(tilePos);
+        deltaPos = mousePosWorld - tilePosWorld;
     }
 
     private void OnEndDrag()
     {
-        //Debug.Log("End drag");
-        DropTile();
-        Destroy(dragPanel);
+        SetDropPosition();
+        transform.position = Position;
     }
+
+    private void SetDropPosition()
+    { 
+        var mousePos = Input.mousePosition;
+        var mousePosWorld = camera.ScreenToWorldPoint(mousePos);
+        var position = mousePosWorld - deltaPos;
+        var hit = Physics2D.Raycast(mousePosWorld, Vector2.zero);
+        if (!hit.collider)
+        {
+            //Debug.Log("no collider");
+            return;
+        }
+        var tile = hit.collider.GetComponent<Tile>();
+        if (tile == null)
+        {
+            return;
+        }
+
+        if (tile.TileType == TileType.Empty)
+        {
+            Position = tile.transform.position;
+        }
+    }
+
     private void OnProcessDrag()
     {
-        //if (CanPlace())
-        {
-            
-        }
-        //else
-        {
-            
-        }
     }
 
-    private void DropTile()
+    private void Update()
     {
-        Vector2 position = transform.localPosition;
-        var zeroPos = Levels.ZeroPos;
-        var sprite = GetComponent<UI2DSprite>();
-        var tileSize = new Vector2(sprite.width,sprite.height);
-        //var tileSize = new Vector2(100,100);
-        var mapSize = new Vector2(Levels.Level3.GetLength(0), Levels.Level3.GetLength(1));
-        position.x += tileSize.x / 2;
-        position.y += tileSize.y / 2;
-        if (position.x > zeroPos.x && position.x < zeroPos.x + mapSize.x * tileSize.x &&
-            position.y > zeroPos.y && position.y < zeroPos.y + mapSize.y * tileSize.y)
-        {
-            var mapPosition = new Vector2Int((int)((position.x - zeroPos.x)/ tileSize.x), (int) ((position.y - zeroPos.y)/ tileSize.y));
-            if (Levels.Level3[4 - mapPosition.y, mapPosition.x].Equals(' '))
-            {
-                Position = new Vector2(zeroPos.x + tileSize.x * mapPosition.x, zeroPos.y + tileSize.y * mapPosition.y);
-            }
-        //} else if (position.x > Levels.ScrollBoundsMin.x && position.x < Levels.ScrollBoundsMax.x &&
-        //           position.y > Levels.ScrollBoundsMin.y && position.y < Levels.ScrollBoundsMax.y)
-        }
-        else
-        {
-            //Position = Levels.ScrollPos;
-            Position = pickPosition;
-            //transform.localPosition = asfasf;
-        }
-
-
-        //Position = asfasf;
-
-        transform.localPosition = Position;
-    }
-
-    private Vector3 pickPosition;
-    private void PickTile()
-    {
-        pickPosition = this.transform.localPosition;
-
-
     }
 }
