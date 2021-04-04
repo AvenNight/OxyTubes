229e7a3a1b1c5b910e71fb204a2aedc1409fb359
Artem Ibragimov <ibragimov@skytecgames.ru>
2021-04-04 23:01:43 +0700

    settings up

--- Assets/Scripts/VerticalLayout/VerticalLayoutEditor.cs
+++ Assets/Scripts/VerticalLayout/VerticalLayoutEditor.cs
@@ -1,22 +1,22 @@
-﻿using GameCore.UI.Layout;
-using UnityEditor;
-using UnityEngine;
+﻿//using GameCore.UI.Layout;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(VerticalLayout))]
-public class VerticalLayoutEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+//[CustomEditor(typeof(VerticalLayout))]
+//public class VerticalLayoutEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
 
-        var layout = (VerticalLayout)target;
-        if (GUILayout.Button("Init Layout")) layout.InitLayoutFromScene();
+//        var layout = (VerticalLayout)target;
+//        if (GUILayout.Button("Init Layout")) layout.InitLayoutFromScene();
 
-        if (GUILayout.Button("Align"))
-        {
-            layout.Align();    
-        }
+//        if (GUILayout.Button("Align"))
+//        {
+//            layout.Align();    
+//        }
 
-        if (GUILayout.Button("Reset layout")) layout.ResetInitialState();
-    }
-}
+//        if (GUILayout.Button("Reset layout")) layout.ResetInitialState();
+//    }
+//}
