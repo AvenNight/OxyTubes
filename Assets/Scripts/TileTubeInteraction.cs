b026fab50daae3487d697c95acd2074c6268f94e
glukonut <razinkov@maxfungames.com>
2021-04-04 18:05:29 +0700

    up

--- Assets/Scripts/TileTubeInteraction.cs
+++ Assets/Scripts/TileTubeInteraction.cs
@@ -13,10 +13,12 @@ public class TileTubeInteraction : MonoBehaviour
     private Vector3 Position;
 
     private Tile curLightTile = null;
+    private Tile curTile = null;
     
     private UIPanel dragPanel;
 
     public Action OnTilePut;
+    private bool moveOnScroll;
 
     public void Start()
     {
@@ -28,10 +30,9 @@ public class TileTubeInteraction : MonoBehaviour
     private void OnStartDrag()
     {
         Position = transform.position;
-        var tile = GetTile();
-        if (tile != null)
+        if (curTile != null)
         {
-            tile.isEnable = true;
+            curTile.isEnable = true;
         }
         // возвышаем выше всего
         dragPanel = this.gameObject.AddComponent<UIPanel>();
@@ -41,7 +42,10 @@ public class TileTubeInteraction : MonoBehaviour
     private void OnEndDrag()
     {
         SetDropPosition();
-        transform.position = Position;
+        var shift = Position - transform.position;
+        GetComponent<Collider>().enabled = false;
+        StartCoroutine(MoveTileTube(shift));
+        OnTilePut?.Invoke();
         if (curLightTile != null)
         {
             curLightTile.TileUnlight();
@@ -51,29 +55,89 @@ public class TileTubeInteraction : MonoBehaviour
         Destroy(dragPanel);
     }
 
+    private IEnumerator MoveTileTube(Vector3 shift, float time = 0.2f)
+    {
+        var curTime = 0f;
+
+        var start = transform.position;
+        var end = Position;
+
+        while (curTime < time)
+        {
+            transform.position = Vector3.Lerp(start, end, curTime / time);
+            curTime += Time.deltaTime;
+            
+            yield return 0;
+        }
+        if (curTime >= time)
+        {
+            transform.position = end;
+            GetComponent<Collider>().enabled = true;
+            if (moveOnScroll)
+            {
+                moveOnScroll = false;
+                //MoveOnScroll();
+                Debug.Log("Ya na scrolle");
+            }
+        }
+        
+    }
     private void SetDropPosition()
     {
-        var tile = GetTile();
-        if (tile == null) return;
-        if (tile.TileType == TileType.Empty && tile.isEnable)
+        var collider = GetTile();
+        Tile tile = null;
+        if (collider != null)
         {
-            Position = tile.transform.position;
-            tile.isEnable = false;
-            OnTilePut?.Invoke();
+            tile = collider.GetComponent<Tile>();
+        }
+        if (collider == null)
+        {
+            if (curTile != null)
+            {
+                curTile.isEnable = false;
+            }
+            else
+            {
+                moveOnScroll = true;
+            }
+        }
+        else if (collider != null && tile == null)
+        {
+            curTile = null;
+            Position = collider.transform.position;
+            moveOnScroll = true;
+        }
+        else if (tile != null)
+        {
+            if (tile.TileType == TileType.Empty && tile.isEnable)
+            {
+                Position = tile.transform.position;
+                tile.isEnable = false;
+                curTile = tile;
+            } else if (!tile.isEnable && curTile != null)
+            {
+                curTile.isEnable = false;
+            }
         }
     }
 
-    private Tile GetTile()
+    private Collider2D GetTile()
     { 
         var mousePos = Input.mousePosition;
         var mousePosWorld = camera.ScreenToWorldPoint(mousePos);
         var hit = Physics2D.Raycast(mousePosWorld, Vector2.zero);
-        return !hit.collider ? null : hit.collider.GetComponent<Tile>();
+        return !hit.collider ? null : hit.collider;
     }
 
     private void OnProcessDrag()
     {
-        var tile = GetTile();
+        var collider = GetTile();
+        Tile tile = null;
+        if (collider != null)
+        {
+            tile = collider.GetComponent<Tile>();
+        }
+        
         if (tile == null )
         {
             if (curLightTile == null)
