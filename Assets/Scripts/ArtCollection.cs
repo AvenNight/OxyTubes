10bc2b46aec3d26155c5fd27dbe76b6725a2c073
Artem Ibragimov <ibragimov@skytecgames.ru>
2021-04-04 14:21:04 +0700

    up

--- Assets/Scripts/ArtCollection.cs
+++ Assets/Scripts/ArtCollection.cs
@@ -7,8 +7,14 @@ public class ArtCollection : MonoBehaviour
 	public static ArtCollection Instance = null;
 
 	public Sprite Empty;
-
+	
 	public List<Sprite> Walls;
+
+	public Sprite Tube1;
+	public Sprite Tube2;
+	public Sprite Tube2Line;
+	public Sprite Tube3;
+	public Sprite Tube4;
 
 	private void Awake()
 	{
--- Assets/Scripts/TileTube.cs
+++ Assets/Scripts/TileTube.cs
@@ -1,6 +1,22 @@
 ﻿using UnityEngine;
 
+public class TileData
+{
+    public bool Up { get; protected set; }
+    public bool Down { get; protected set; }
+    public bool Left { get; protected set; }
+    public bool Right { get; protected set; }
+}
+
 public class TileTube : MonoBehaviour
 {
-  
+    public UISprite Sprite;
+    public TileData Data { get; protected set; }
+
+    public void Set(TileData data)
+    {
+        Data = data;
+
+
+    }
 }
